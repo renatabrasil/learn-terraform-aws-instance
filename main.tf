31c3b460942818caebcae0f67a1515ba0d5c4ac9
Renata Brasil <renatabbss@gmail.com>
2021-09-04 21:17:28 -0300

    Configurando o terracloud

--- main.tf
+++ main.tf
@@ -1,4 +1,10 @@
 terraform {
+  backend "remote" {
+    organization = "renata-corporation"
+    workspaces {
+      name = "Example-Workspace"
+    }
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -15,10 +21,10 @@ provider "aws" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
+  ami           = "ami-08d70e59c07c61a3a"
   instance_type = "t2.micro"
 
   tags = {
-    Name = "ExampleAppServerInstance"
+    Name = var.instance_name
   }
 }
