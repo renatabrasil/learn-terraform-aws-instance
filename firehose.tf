10e2ee86228b8872251227325bd0e9e12c428780
Renata Brasil <renatabbss@gmail.com>
2022-01-23 22:45:16 -0300

    Subir o bucket e habilitar dynamodb stream

--- firehose.tf
+++ firehose.tf
@@ -1,5 +1,5 @@
 module "kinesis-firehose" {
-  source                                = "felipefrizzo/kinesis-firehose/aws"
+  source                                = "git::https://github.com/felipefrizzo/terraform-aws-kinesis-firehose.git?ref=v0.0.4"
   region                                = "us-west-2"
   kinesis_firehose_stream_name          = "music_stream"
   kinesis_firehose_stream_backup_prefix = "bck"
