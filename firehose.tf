a1c68efb1d5d2a66765ac942e7e143ee3321d167
Renata Brasil <renatabbss@gmail.com>
2022-01-23 23:27:12 -0300

    Subir o bucket e habilitar dynamodb stream

--- firehose.tf
+++ firehose.tf
@@ -15,18 +15,18 @@ resource "aws_kinesis_firehose_delivery_stream" "extended_s3_stream" {
     role_arn   = aws_iam_role.firehose_role.arn
     bucket_arn = aws_s3_bucket.music_s3.arn
 
-//    processing_configuration {
-//      enabled = "true"
-//
-//      processors {
-//        type = "Lambda"
-//
-//        parameters {
-//          parameter_name  = "LambdaArn"
-//          parameter_value = "${aws_lambda_function.lambda_processor.arn}:$LATEST"
-//        }
-//      }
-//    }
+    //    processing_configuration {
+    //      enabled = "true"
+    //
+    //      processors {
+    //        type = "Lambda"
+    //
+    //        parameters {
+    //          parameter_name  = "LambdaArn"
+    //          parameter_value = "${aws_lambda_function.lambda_processor.arn}:$LATEST"
+    //        }
+    //      }
+    //    }
   }
 }
 
--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ terraform {
   }
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
+      source = "hashicorp/aws"
     }
   }
 
